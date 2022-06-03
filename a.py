4c31bbd6839754f4bc5d0a834af1230c967a5dca
jeong59 <new2694@kaist.ac.kr>
2022-06-03 23:50:38 +0900

    led and upload

--- a.py
+++ a.py
@@ -7,6 +7,5 @@ while(True):
     t = Timer(timeout, print, ['Sorry, times up'])
     t.start()
     time.sleep(2)
-    t.cancel()
     print('1')
-    
+    t.cancel()
--- firebase.py
+++ firebase.py
@@ -1,13 +1,11 @@
 import firebase_admin
 import serial
-import random
 import time
 from firebase_admin import credentials
-from firebase_admin import db
 from firebase_admin import firestore
 
 class Position:
-    def __init__(self, latitude = 0, longitude = 0, t = ''):
+    def __init__(self, latitude = 0, longitude = 0, t = 0):
         self.latitude = latitude
         self.longitude = longitude
         self.time = t
@@ -16,12 +14,12 @@ class Position:
         dest = {
             u'created_at': self.time,
             u'latitude': self.latitude,
-            u'longitude': self.longitude
+            u'longitude': self.longitude,
         }
         return dest
 
 class Heart:
-    def __init__(self, heart_rate = 0, t = ''):
+    def __init__(self, heart_rate = 0, t = 0):
         self.heart_rate = heart_rate
         self.time = t
 
@@ -33,7 +31,7 @@ class Heart:
         return dest
 
 class Light:
-    def __init__(self, inpersity = 0, t = ''):
+    def __init__(self, inpersity = 0, t = 0):
         self.inpersity = inpersity
         self.time = t
 
@@ -45,7 +43,7 @@ class Light:
         return dest
 
 class Breath:
-    def __init__(self, breath = 0, t = ''):
+    def __init__(self, breath = 0, t = 0):
         self.breath = breath
         self.time = t
 
@@ -66,30 +64,54 @@ ser = serial.Serial(
     baudrate=9600,
 )
 
-while (ser.readable()):
-    data = ser.readline().decode().strip(',')
-    t = time.strftime('%X')
+count = 0
+while (count != 100):
+    data = ser.readline().decode().split(',')
+
+    heartrate = float(data[1])
+    lat = float(data[3])
+    lon = float(data[5])
+    luxlen = len(data[7])
+    lux = float(data[7][0: luxlen-2])
+
+    t1 = time.time()
 
     db1 = firestore.client()
     heart_col = db1.collection(u'heart sensor')
-    heart = Heart(u'%d' %1, t)
+    heart = Heart(u'%.3f' %heartrate, t1)
     heart_col.add(heart.to_dict())
 
-    # db2 = firestore.client()
-    # breath_col = db2.collection(u'breath sensor').document(u'a').set({'1':2})
-    # breath = Breath(latitude= u'%d' %1, longitude = u'a', t = time.strftime('%X'))
-    # breath_col.add(breath.to_dict())
+    low_value = 60
+    high_value = 120
+    color = ''
+    if (low_value >= heartrate or heartrate >= high_value) :
+        color = 'R'
+    else:
+        color = 'G'
+    color = color.encode('utf-8')
+    ser.write(color)
 
-    # db3 = firestore.client()
-    # light_col = db3.collection(u'light sensor').document(u'a').set({'1':2})
-    # light = Light(latitude= u'%d' %1, longitude = u'a', t = time.strftime('%X'))
-    # light_col.add(light.to_dict())
+    db2 = firestore.client()
+    breath_col = db2.collection(u'breath sensor')
+    breath = Breath(u'%.3f' %breath, t1)
+    breath_col.add(breath.to_dict())
+
+    db3 = firestore.client()
+    light_col = db3.collection(u'light sensor')
+    light = Light(u'%.3f' %lux, t1)
+    light_col.add(light.to_dict())
 
     db4 = firestore.client()
     GPS_col = db4.collection(u'GPS sensor')
-    pos = Position(u'%d' %1, u'a', t)
+    pos = Position(u'%.3f' %lat, u'%.3f' %lon, t1)
     GPS_col.add(pos.to_dict())
 
-    time.sleep(1)
+    t2 = time.time()
+
+    count += 1
+    if(t2-t1 > 1):
+        continue
+    else:
+        time.sleep(1-(t2-t1))
 
 #'databaseURL' : 'https://smart-pet-collar-4am-default-rtdb.firebaseio.com'
